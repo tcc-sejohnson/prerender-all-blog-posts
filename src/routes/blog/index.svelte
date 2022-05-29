d072bcbf538d815213974ef1e3c4629b8c29c256
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-05-29 13:05:45 -0600

    feat: Remove links on index page

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
+	import { prerendering } from '$app/env';
 	// If you don't want an "index" page with the list of links,
 	// just add "visibility: hidden" to the div wrapper around the anchor tags.
 
 	export let postRoutes: Array<string>;
+	export const prerender = false;
 </script>
 
-<div>
-	<ul>
-		{#each postRoutes as route}
-			<li><a href={`blog/${route}`}>{route}</a></li>
-		{/each}
-	</ul>
-</div>
+{#if prerendering}
+	<div>
+		<ul>
+			{#each postRoutes as route}
+				<li><a href={`blog/${route}`}>{route}</a></li>
+			{/each}
+		</ul>
+	</div>
+{/if}
 
 <style>
 	div {
-		/* visibility: hidden; */
+		visibility: hidden;
 		display: flex;
 		flex-direction: column;
 	}
